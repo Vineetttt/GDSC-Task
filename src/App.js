87314cbf18960ffbc94a83e23c9dc799ad783cac
Vineetttt <vineetchotaliya30@gmail.com>
2022-09-23 23:15:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import {useState, useEffect } from "react";
+import './App.css'
+import Quiz from "./quiz";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getData = "https://opentdb.com/api.php?amount=10";
+function App(){
+    const [questions,getQuestion] = useState([]);
+    const [index,currentIndex] = useState(0);
+    const [score,updateScore] = useState(0);
+
+    useEffect(() =>{
+      axios.get(getData)
+      .then(response => response.data)
+      .then(data => {
+        //console.log(data.results)
+
+        // to shuffel the correct answer create a new array with all the answers including incorrect
+        const questions = data.results.map((question) => ({
+          ...question, //not chnaging the data except 
+          //sorting the answer array so the correct answer is not at the same index always 
+          answers: [question.correct_answer,...question.incorrect_answers].sort(() => Math.random() - 0.5) 
+        }))
+        console.group(questions)
+        getQuestion(questions)  
+      })
+},[])
+
+const checkAnswer = (answer) =>{
+  //checking i fthe answer matches the correct answer from api
+  if(answer === questions[index].correct_answer){
+    updateScore(score+1);
+  }
+  currentIndex(index+1)
+  console.log("Your score is ",score);
 }
+    return(
+       questions.length > 0 ? (
+        <div className="container">
+          {index >= questions.length ? (
+            <h1 className="final-score">Final Score: {score}</h1>
+          ):(
+            <Quiz checkAnswer = {checkAnswer}
+            score = {score}
+            data={questions[index]}/>
+          )}
+        </div>
+    ) :"Fetching...."
+
+    );
+};
 
 export default App;
---
+++ src/quiz.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Quiz({score,checkAnswer,data:{question,correct_answer,answers}}){
+    return(
+        <>
+        <div className="questionClass">
+                <h1 dangerouslySetInnerHTML={{__html:question}} />
+            </div>
+        <div className="optionClass">
+            {answers.map((answer,i) => {
+                return(
+                    <button className="button" 
+                    onClick = {() => checkAnswer(answer)}
+                    dangerouslySetInnerHTML={{__html:answer}} />
+                )
+            })}
+        </div>
+        <p className="scr">Score: &nbsp;<span dangerouslySetInnerHTML={{__html:score}}/></p>
+        </>
+    )
+}
+export default Quiz;
