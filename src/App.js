f70bc0fe88146a3ca2221df7f9ad671725eba63f
Vineetttt <vineetchotaliya30@gmail.com>
2022-09-27 01:32:53 +0530

    final-commit

--- src/App.js
+++ src/App.js
@@ -6,38 +6,41 @@ import Quiz from "./AppComponents/quiz";
 
 const getData = "https://opentdb.com/api.php?amount=10";
 function App(){
-    const [questions,getQuestion] = useState([]);
-    const [index,currentIndex] = useState(0);
-    const [score,updateScore] = useState(0);
+    const [questions,getQuestion] = useState([]); // initializing questions with an empty array
+    const [index,currentIndex] = useState(0);     // initializing with first index 0 
+    const [score,updateScore] = useState(0);      // initial score is set to 0 
 
+    // use effect hook is used to perform side tasks like fetching data 
     useEffect(() =>{
       axios.get(getData)
       .then(response => response.data)
       .then(data => {
-        //console.log(data.results)
 
-        // to shuffel the correct answer create a new array with all the answers including incorrect
+        // to shuffel the correct answer to a new place every time create a new object with all the answers including incorrect
         const questions = data.results.map((question) => ({
-          ...question, //not chnaging the data except 
+          ...question,  
           //sorting the answer array so the correct answer is not at the same index always 
           answers: [question.correct_answer,...question.incorrect_answers].sort(() => Math.random() - 0.5) 
         }))
-        console.log(questions)
+        //console.log(questions) //will display the questions and corresponding correct answers in console
         getQuestion(questions)  
       })
 },[])
 
 const checkAnswer = (answer) =>{
-  //checking i fthe answer matches the correct answer from api
+  //checking if the user selected answer matches the correct answer from api data
   if(answer === questions[index].correct_answer){
     updateScore(score+1);
   }
   currentIndex(index+1)
-  console.log("Your score is ",score);
+  console.log("Your current score is ",score);
 }
+
     return(
+       /*if some data is fetched i.e. the size of the array is > 0 then call the fucntion else display still fetching the data*/
        questions.length > 0 ? (
         <div className="container">
+          {/*when there are no questions left to display output the final quiz score*/}
           {index >= questions.length ? (
             <>
             <h1 className="final-score">Final Score: {score}</h1>
@@ -45,6 +48,7 @@ const checkAnswer = (answer) =>{
             </>
           ):(
             <>
+            {/*Passing the data fetched into quiz.js to display it on the page */}
             <Quiz checkAnswer = {checkAnswer}
             score = {score}
             data={questions[index]} />
--- src/AppComponents/quiz.js
+++ src/AppComponents/quiz.js
@@ -1,19 +1,23 @@
 import React from "react";
 
-function Quiz({score,checkAnswer,data:{question,correct_answer,answers}}){
+//de-structuring the array data inside the fucntion to use 
+function Quiz({score,checkAnswer,data:{question,answers}}){
     return(
         <>
         <div className="questionClass">
                 <h1 dangerouslySetInnerHTML={{__html:question}} />
             </div>
         <div className="optionClass">
+            {/*number of options visible is equal to the size of the answers array*/}
             {answers.map((answer,i) => {
                 return(
                     <button className="button" 
                     onClick = {() => checkAnswer(answer)}
                     dangerouslySetInnerHTML={{__html:answer}} />
                 )
-            })}
+            })
+            
+            }
         </div>
         <p className="button score">Score:<span dangerouslySetInnerHTML={{__html:score}}/></p>
         </>
